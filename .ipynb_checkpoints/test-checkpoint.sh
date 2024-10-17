71b035435c1b1d76449ad0ea99dc7ea04ceee8fe
saranrt95 <77918497+saranrt95@users.noreply.github.com>
2024-10-17 18:29:44 +0200

    Delete .ipynb_checkpoints directory

--- .ipynb_checkpoints/test-checkpoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for dataset in $(echo 'p2p' 'ssh' 'smoking' 'cardio' 'platooning' 'rul' 'eeg' 'mqttset' 'telescope' 'fire'); do #'ssh'
-	for typetest in $(echo 'true');do
-		(python3 ConformalLLM.py $dataset $typetest 1)
-	done
-done
